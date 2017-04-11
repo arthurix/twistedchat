73bc724acada831bd1eb8936476d82d406e1b270
Arthur Oliveira <arthuryx@gmail.com>
2017-04-11 15:52:03 +0000

    Adjust layout and chat scrolling

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,9 +1,20 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {  
   received: function(data) {
-    $("#messages").removeClass('hidden')
-    return $('#messages').append(this.renderMessage(data));
+    $("#messages").removeClass('hidden');
+    this.scrollDown("#messages");
+    $('#messages').append(this.renderMessage(data));
+    return this.MessageAnimation("#messages");
   },
   renderMessage: function(data) {
-    return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+    return "<p><b>" + data.user + ": </b><span>" + data.message + "</span></p>";
+  },
+  scrollDown: function(elem) {
+    var scroller = $(elem);
+    var height = scroller[0].scrollHeight - $(scroller).height() + 35;
+
+    $(scroller).stop().animate({ scrollTop: height }, "slow");
+  },
+  MessageAnimation: function(elem){
+      $(elem).children("p:last").hide().effect("highlight", {color:"#FFFCCF"}, 1500);
   }
 });
